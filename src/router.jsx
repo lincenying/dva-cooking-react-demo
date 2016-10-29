261ba3aa8edde82ed8fef085a0371848a3850a5e
lincenying <lincenying@qq.com>
2016-10-29 20:32:31 +0800

    :+1:

--- src/router.jsx
+++ src/router.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { Router, Route } from 'dva/router'
-
-const routerThen = (app, callback, [component, model]) => {
-    app.model(model.default || model)
-    callback(null, component.default || component)
-}
-
-export default ({ history, app }) => {
-    return (
-        <Router history={history}>
-            <Route name="home" path="/" getComponent={(location, callback) => {
+import {Router} from 'dva/router'
+export default({history, app}) => {
+    const routerThen = (callback, [component, model,]) => {
+        app.model(model.default || model)
+        callback(null, component.default || component)
+    }
+    const routeConfig = [
+        {
+            name: 'home',
+            path: '/',
+            getComponent(location, callback) {
                 Promise.all([
-                    System.import('./views/app.jsx'),
-                    System.import('./models/topics')
-                ]).then(routerThen.bind(null, app, callback))
-            }} />
-            <Route name="article" path="/article/:id" getComponent={(location, callback) => {
+                    System.import ('./views/app.jsx'),
+                    System.import ('./models/topics'),
+                ]).then(routerThen.bind(null, callback))
+            }
+        }, {
+            name: 'article',
+            path: '/article/:id',
+            getComponent(location, callback) {
                 Promise.all([
-                    System.import('./views/article.jsx'),
-                    System.import('./models/topic')
-                ]).then(routerThen.bind(null, app, callback))
-            }} />
-        </Router>
-    )
+                    System.import ('./views/article.jsx'),
+                    System.import ('./models/topic'),
+                ]).then(routerThen.bind(null, callback))
+            }
+        },
+    ]
+    return <Router history={history} routes={routeConfig} />
 }
